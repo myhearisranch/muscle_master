cb989bc417683681df2d70d5f9afe7dfd9c7b5f4
root <root@DESKTOP-2O6QUN5>
2024-02-17 18:14:40 +0900

    #10 ListView

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -1,81 +1,105 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-main() {
+//モデルを用意する(Flutterだとクラスを用意すればいい)
+
+//ツイート(一つのデータに必要なデータを決めた)
+class Tweet{
+
+  //ユーザーの名前
+  final String userName;
+
+  //アイコン画像
+  final String iconUrl;
+
+  //文章メッセージ
+  final String text; 
+
+  //送信日時
+  final String createdAt;
+
+  //コンストラクタ
+  Tweet(this.userName, this.iconUrl, this.text, this.createdAt);
+}
+
+// 適当なモデル
+final models = [
+  Tweet('ルフィ', 'icon1.png', '海賊王におれはなる！', '2022/1/1'),
+  Tweet('ゾロ', 'icon2.png', 'おれはもう！二度と敗けねェから！', '2022/1/2'),
+  Tweet('ナミ', 'icon3.png', 'もう背中向けられないじゃないっ！', '2022/1/3'),
+  Tweet('ウソップ', 'icon4.png', 'お前らの”伝説のヒーロー”になってやる！', '2022/1/4'),
+  Tweet('サンジ', 'icon5.png', 'たとえ死んでもおれは女は蹴らん・・・！', '2022/1/5'),
+  Tweet('チョッパー', 'icon6.png', '人間ならもっと自由だ！', '2022/1/6'),
+  Tweet('ビビ', 'icon7.png', 'もう一度仲間と呼んでくれますか!?', '2022/1/7'),
+  Tweet('ロビン', 'icon8.png', '生ぎたいっ！', '2022/1/8'),
+  Tweet('フランキー', 'icon9.png', '存在することは罪にはならねェ！', '2022/1/9'),
+  Tweet('ブルック', 'icon10.png', '男が一度・・・必ず帰ると言ったのだから！', '2022/1/10'),
+  Tweet('ジンベイ', 'icon11.png', '失ったものばかり数えるな！', '2022/1/11'),
+  Tweet('シャンクス', 'icon1.png', 'この帽子をお前に預ける', '2022/2/1'),
+  Tweet('ヒルルク', 'icon2.png', '違う!…人に忘れられた時さ…!', '2022/2/2'),
+  Tweet('ドクタークレハ', 'icon3.png', '優しいだけじゃ人は救えないんだ!', '2022/2/3'),
+  Tweet('ティーチ', 'icon4.png', '人の夢は!終わらねェ!', '2022/2/4'),
+  Tweet('ガンフォール', 'icon5.png', '人の生きるこの世界に“神”などおらぬ!', '2022/2/5'),
+  Tweet('ボンクレー', 'icon6.png', '理由なんざ他にゃいらねェ!', '2022/2/6'),
+  Tweet('イワンコフ', 'icon7.png', '“奇跡”ナメるんじゃないよォ!', '2022/2/7'),
+  Tweet('ニューゲート', 'icon8.png', 'バカな息子をそれでも愛そう・・・', '2022/1/8'),
+  Tweet('エース', 'icon9.png', '愛してくれて・・・ありがとう', '2022/2/9'),
+  Tweet('ロー', 'icon10.png', '取るべきイスは…必ず奪う!', '2022/2/10'),
+  Tweet('サボ', 'icon11.png', '以後ルフィのバックにはおれがついてる!', '2022/2/11'),
+  Tweet('バルトロメオ', 'icon1.png', 'この子分盃!勝手に頂戴いたしますだべ!', '2022/3/1'),
+];
+
+//モデルをウィジェットに変換する関数を作成する
+//引数をTweet modelとすることで上で作ったTweetのモデルを使うことができる、
+Widget modelToWidget(Tweet model) {
+  // ユーザーアイコン
+  final icon = Container(
+    margin: const EdgeInsets.all(20),
+    width: 60,
+    height: 60,
+    // 画像を丸くする
+    child: ClipRRect(
+      borderRadius: BorderRadius.circular(30.0),
+      child: Image.asset('assets/images/${model.iconUrl}'),
+    ),
+  );
+
+  //アイコンウィジェットを返す
+  return icon;
+
+  //これを書かないとflutter run -d web-server --web-port=8889 --web-hostname 0.0.0.0とした時、下記のようなエラーが発生する
+  //   lib/main.dart:53:8: Error: A non-null value must be returned since the return type 'Widget' doesn't allow null.
+  //  - 'Widget' is from 'package:flutter/src/widgets/framework.dart' ('../flutter/packages/flutter/lib/src/widgets/framework.dart').
+  //   Widget modelToWidget(Tweet model) {
+  //       ^
+  //   Waiting for connection from debug service on Web Server...         94.5s
+  //   Failed to compile application.
+  
+}
+
+void main() {
+  // ツイートのリスト
+  final list = ListView.builder(
+    itemCount: models.length,
+    itemBuilder: (c, i) => modelToWidget(models[i]),
+  );
+
+  final con = Center(
+    child: SizedBox(
+      height: 400,
+      child: list,
+    ),
+  );
+
+  // 画面
+  final sca = Scaffold(
+    backgroundColor: Colors.grey,
+    body: con,
+  );
+
   // アプリ
-  const app = MaterialApp(home: Example());
-
-  // アプリ全体をプロバイダースコープというもので囲む必要がある
-  const scope = ProviderScope(child: app);
-  runApp(scope);
-
+  final app = MaterialApp(home: sca);
+  // アプリを動かす
+  runApp(app);
 }
 
-//　プロバイダー
-final nicknameProvider = StateProvider<String>(
-  (ref){
 
-    //変化するデータ
-    return "ルビードッグ";
-  }
-);
-
-//riverpodの仕組み
-
-// 1 変化させたいデータをProviderという壁で守る
-// 2 画面は特別なConsumerWidgetを使う
-// 3 ConsumerWidgetを使うことでrefというものが手に入る
-// 4 refとは、Providerの鍵のようなもの => refを使うで画面の方からProviderの中にあるデータを監視できる
-// 5 Notifier: Providerの中のデータを変更する => refを使ってデータを監視していた画面にデータの変更が反映される
-
-// 画面
-class Example extends ConsumerWidget {
-  const Example({super.key});
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-   
-   //データを見張る(ref)
-   final nickname = ref.watch(nicknameProvider);
-
-    return Scaffold(
-      // ニックネーム 1
-      appBar: AppBar(title: Text(nickname)),
-      body: SizedBox(
-        width: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            // ニックネーム 2
-            Text(nickname),
-            // ボタン A
-            ElevatedButton(onPressed: () => tapA(ref), child: const Text('A')),
-            // ボタン B
-            ElevatedButton(onPressed: () => tapB(ref), child: const Text('B')),
-            // ボタン C
-            ElevatedButton(onPressed: () => tapC(ref), child: const Text('C')),
-            // ニックネーム 3
-            Text(nickname),
-          ],
-        ),
-      ),
-    );
-  }
-
-  //notifieでデータを編集する
-  tapA(WidgetRef ref) {
-    final notifier = ref.read(nicknameProvider.notifier);
-    notifier.state = 'ルビーキャット';
-  }
-
-  tapB(WidgetRef ref) {
-    final notifier = ref.read(nicknameProvider.notifier);
-    notifier.state = 'ルビーバード';
-  }
-
-  tapC(WidgetRef ref) {
-    final notifier = ref.read(nicknameProvider.notifier);
-    notifier.state = 'ルビーフィッシュ';
-  }
-}
