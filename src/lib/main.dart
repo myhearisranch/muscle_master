ccf3d924c9104a1cfa9b447cf149a5b249874b48
root <root@DESKTOP-2O6QUN5>
2024-02-23 11:57:07 +0900

    #13 BottomNavigationBar

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -1,49 +1,154 @@
 import 'package:flutter/material.dart';
-import 'package:banana/part12/side_menu.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'page_a.dart';
+import 'page_b.dart';
+import 'page_c.dart';
+
+// // プロバイダー
+// final indexProvider = StateProvider((ref){
+//   //変化させたいデータ
+//   //スタートのインデックスとなる 0はindexの番号
+//   return 0;
+// },);
+
+// class Root extends ConsumerWidget{
+//   const Root({super.key})
+
+//   @override
+//   Widget build(BuildContext context, WidgetRef ref){
+
+//     //右側のProviderというのがとれて、左側にindexとしてゲットできる。
+//     final index = ref.watch(indexProvider);
+
+//     //アイテムたち(ナビゲーションバーにあるアイコンと名前をセットにしたもの)
+//     //BottomNavigationBarItem: アイテム専用のウィジェット
+//     //使い方
+//     // BottomNavigationBarItem(
+//     //   icon: アイコン
+//     //   label: 文字
+//     // )
+
+//     const items = [
+
+//       BottomNavigationBarItem(
+//         icon: Icon(Icons.person),
+//         label: 'アイテムA'
+//       ),
+
+//       BottomNavigationBarItem(
+//         icon: Icon(Icons.home),
+//         label: 'アイテムB'
+//       ),
+
+//       BottomNavigationBarItem(
+//         icon: Icon(Icons.settings),
+//         label: 'アイテムC'
+//       ),
+
+//     ];
+
+//     //itemsを使って、ナビゲーションバー全体を作成している
+//     final bar = BottomNavigationBar(
+
+//       //使うitemを決める
+//       items: items,
+//       //ナビゲーションバーの背景の色を決める
+//       backgroundColor: Colors.red,
+//       //押されたアイテムの色を決める
+//       selectedItemColor: Colors.white,
+//       //押されたアイテム以外のアイテムの色を決める
+//       unselectedItemColor: Colors.black,
+//       currentIndex: index,
+
+//       //タップされた時にindexを変更するための状態管理のコード
+//       onTap: (index) {
+//         ref.read(indexProvider.notifier).state = index;
+//       },
+
+//     );
+
+//     //上の画面全体を一つのリストに入れておく
+//     final pages = [
+//       PageA(),
+//       PageB(),
+//       PageC(),
+//     ];
+
+//     return Scaffold(
+//       body: pages[index],
+//       bottomNavigationBar: bar
+//     );
+
+//     return Container();
+//   }
+// }
+
 
 main() {
-  // アップバー
-  final appBar = AppBar(
-    title: const Text('appBar'),
-  );
-
-  // ドロワー
-  const drawer = Drawer(
-    child: SideMenu(),
-  );
-
-  // エンドドロワー
-  const endDrawer = Drawer(
-    child: SideMenu(),
-  );
-
-  // フローティングアクションボタン (FAB)
-  final fab = FloatingActionButton(
-    onPressed: () {
-      debugPrint('FAB が押されました');
-    },
-    child: const Text('FAB'),
-  );
-
-  // ボディ
-  const body = Center(
-    child: Text('body'),
-  );
-
-  // 画面
-  final scaffold = Scaffold(
-    appBar: appBar, // アップバー
-    drawer: drawer, // ドロワー
-    endDrawer: endDrawer, // エンドドロワー
-    floatingActionButton: fab, // フローティングアクションボタン (FAB)
-    body: body, // ボディ
-  );
-
   // アプリ
-  final app = MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: scaffold,
-  );
+  const app = MaterialApp(home: Root());
 
-  runApp(app);
+  // プロバイダースコープでアプリを囲む
+  const scope = ProviderScope(child: app);
+  runApp(scope);
 }
+
+// プロバイダー
+final indexProvider = StateProvider((ref) {
+  // 変化するデータ 0, 1, 2...
+  return 0;
+});
+
+// 画面全体
+class Root extends ConsumerWidget {
+  const Root({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // インデックス
+    final index = ref.watch(indexProvider);
+
+    // アイテムたち
+    const items = [
+      BottomNavigationBarItem(
+        icon: Icon(Icons.person),
+        label: 'アイテムA',
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.home),
+        label: 'アイテムB',
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.settings),
+        label: 'アイテムC',
+      ),
+    ];
+
+    // 下のバー
+    final bar = BottomNavigationBar(
+      items: items, // アイテムたち
+      backgroundColor: Colors.red, // バーの色
+      selectedItemColor: Colors.white, // 選ばれたアイテムの色
+      unselectedItemColor: Colors.black, // 選ばれていないアイテムの色
+      currentIndex: index, // インデックス
+      onTap: (index) {
+        // タップされたとき インデックスを変更する
+        ref.read(indexProvider.notifier).state = index;
+      },
+    );
+
+    // 画面たち
+    const pages = [
+      PageA(),
+      PageB(),
+      PageC(),
+    ];
+
+    return Scaffold(
+      body: pages[index],
+      bottomNavigationBar: bar,
+    );
+  }
+}
+
+
