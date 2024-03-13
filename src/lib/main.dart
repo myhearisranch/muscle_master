f66149a9d0abe0b5a48ae2bfa122a28c42d1eb6a
root <root@DESKTOP-2O6QUN5>
2024-03-14 00:06:09 +0900

    #17 freezed

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -1,17 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:percent_indicator/percent_indicator.dart';
+import 'package:banana/part17/abc_list.dart';
 
+// 自分が作った fish.dart ファイルを読み込む
+import 'package:banana/part17/fish.dart';
+
+// 自分が作った abc_list.dart ファイルを読み込む
+
+// メイン関数
 void main() {
   const app = MaterialApp(home: Home());
   const scope = ProviderScope(child: app);
   runApp(scope);
 }
 
-//どのくらい進んだかを表す パーセント
-final percentProvider = StateProvider((ref){
-  //最初は0%からスタート
-  return 0.00;
+// 魚データの状態管理
+final fishProvider = StateProvider((ref) {
+  return const Fish(
+    name: 'マグロ',
+    size: 200,
+    price: 300,
+  );
+});
+
+// ABCリストの状態管理
+final abcListProvider = StateProvider((ref) {
+  return AbcList([
+    'A',
+    'B',
+    'C',
+  ]);
 });
 
 // ホーム画面
@@ -20,106 +38,83 @@ class Home extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    // 魚データ
+    final fish = ref.watch(fishProvider);
+    // ABCリスト
+    final abcList = ref.watch(abcListProvider);
 
-    //パーセント
-    final percent = ref.watch(percentProvider);
-
-    //丸型のインジケーター
-    final circular = CircularPercentIndicator(
-      //変化していく値を指定
-      percent: percent,
-      //丸型インジケーターの全体の色
-      backgroundColor: Colors.yellow,
-      //値が変化していくことで範囲が変わる色
-      progressColor: Colors.green,
-      //円の大きさ
-      radius: 60,
-      //円の太さ
-      lineWidth: 20,
-      //インジケーターの真ん中に文字を置く
-      center: Text('${percent * 100}%'),
-
-
-      //インジケーターに伸びる動きを付けられる
-      animation: true,
-      //インジケーターの動く速さを変える
-      animationDuration: 200,
-      //最初の部分から動きを見せるかがをtrueかfalseで決める
-      animateFromLastPercent: true,
+    // 名前テキスト
+    final nameText = Text(
+      '名前: ${fish.name}',
     );
 
-    //棒型のインジケーター
-    final linear = LinearPercentIndicator(
-      percent: percent,
-      backgroundColor: Colors.yellow,
-      progressColor: Colors.green,
-      alignment: MainAxisAlignment.center,
-      lineHeight: 20,
-      width: 300,
+    // 大きさテキスト
+    final sizeText = Text(
+      '大きさ: ${fish.size} cm',
     );
 
-    //ボタン
-   final button = ElevatedButton(
+    // 値段テキスト
+    final priceText = Text(
+      '値段: ${fish.price} 万円',
+    );
+
+    // ABCリストテキスト
+    final abcListText = Text(
+      'ABCリスト: $abcList',
+    );
+
+    // ボタン
+    final button = ElevatedButton(
       onPressed: () => onPressed(ref),
-      child: const Text('スタート'),
+      child: const Text('変更する'),
     );
 
-
-    //縦に並べるカラム
+    // 縦に並べるカラム
     final column = Column(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
-        circular,
-        linear,
+        nameText,
+        sizeText,
+        priceText,
+        abcListText,
         button,
       ],
     );
 
+    // 画面の真ん中にカラムを置く
     return Scaffold(
       body: Center(
-      child:column,
+        child: column,
       ),
     );
   }
 
-  //ボタンを押した時の関数
-    void onPressed(WidgetRef ref) async {
-      //1秒待つ
-      await Future.delayed(const Duration(seconds: 1));
-      //20%
-      ref.read(percentProvider.notifier).state = 0.20;
-      //1秒待つ
-      await Future.delayed(const Duration(seconds: 1));
-      //40%
-      ref.read(percentProvider.notifier).state = 0.40;
-      //1秒待つ
-      await Future.delayed(const Duration(seconds: 1));
-      //60%
-      ref.read(percentProvider.notifier).state = 0.60;
-      //1秒待つ
-      await Future.delayed(const Duration(seconds: 1));
-      //80%
-      ref.read(percentProvider.notifier).state = 0.80;
-      //1秒待つ
-      await Future.delayed(const Duration(seconds: 1));
-      //100%
-      ref.read(percentProvider.notifier).state = 1.00;
-    }
+  // ボタンを押したときの関数
+  void onPressed(WidgetRef ref) {
+    // 今画面に出ている魚
+    final fish = ref.read(fishProvider);
+
+    // 入れ物ごと変えた 新しい魚(魚、全体の部分の箱も新しく作り変えている)
+    final newFish = fish.copyWith(
+      // 値段は 500 にする
+      price: 500,
+    );
+
+    // 画面を変更する
+    ref.read(fishProvider.notifier).state = newFish;
+
+    // --- --- --- ---
+
+    // 今画面に出ているABCリスト
+    final abcList = ref.read(abcListProvider);
+
+    // 入れ物ごと変えた 新しいABCリスト
+    final newAbcList = abcList.copyWith(
+      // D を追加する
+      values: abcList.values + ['D'],
+    );
+
+    // 画面を変更する
+    ref.read(abcListProvider.notifier).state = newAbcList;
+  }
 }
-
-
-// エラー1:
-
-// lib/main.dart:26:22: Error: The method 'CircularPercentIndicator' isn't defined for the class 'Home'.
-//  - 'Home' is from 'package:workspace/main.dart' ('lib/main.dart').
-// Try correcting the name to the name of an existing method, or defining a method named 'CircularPercentIndicator'.
-//     final circular = CircularPercentIndicator(
-//                      ^^^^^^^^^^^^^^^^^^^^^^^^
-// lib/main.dart:40:20: Error: The method 'LinearPercentIndicator' isn't defined for the class 'Home'.
-//  - 'Home' is from 'package:workspace/main.dart' ('lib/main.dart').
-// Try correcting the name to the name of an existing method, or defining a method named 'LinearPercentIndicator'.
-//     final linear = LinearPercentIndicator(
-//                    ^^^^^^^^^^^^^^^^^^^^^^
-
-//原因:
-//main.dartにimport 'package:percent_indicator/percent_indicator.dart';を書いていなかった
